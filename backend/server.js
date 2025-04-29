d24a93b74554a850d334748fb83c3655709b5e2e
Anuar <a.bayakhmetov@kdss.tech>
2025-04-30 00:12:53 +0500

    feat: changes gitlab ci

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ const trackingRoutes = require('./routes/tracking');
 const adminRoutes = require('./routes/admin');
 
 
+
 // Import middlewares
 const { errorHandler, notFoundHandler } = require('./middleware/errorHandlers');
 const { requestLogger } = require('./middleware/loggers');
